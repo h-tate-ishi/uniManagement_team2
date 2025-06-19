ac66e8fff5e7a957d436a0a4dbd493ae94f465b7
h-tate-ishi <h-tateishi@sr-net.co.jp>
2025-06-19 11:13:58 +0900

    Add files via upload

---
+++ ProductUpdateServlet.java
@@ -0,0 +1,14 @@
+package servlet;
+
+import jakarta.servlet.ServletException;
+import jakarta.servlet.annotation.WebServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+
+@WebServlet(/productupdate)
+public class ProductUpdateServlet {
+	public void doGet (HttpServletRequest request,HttpServletResponse response)
+		throws ServletException,IoException;
+	
+	
+}
